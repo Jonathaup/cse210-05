fe5104f7dafd32087c1036919c3ff9454f513d30
crossproject <97067726+crossproject@users.noreply.github.com>
2022-06-21 11:56:37 -0300

    Simplified controls

--- game/scripting/twoPlayerControl.py
+++ game/scripting/twoPlayerControl.py
@@ -3,10 +3,12 @@ import constants
 from game.shared.point import Point
 from game.scripting.control_actors_action import ControlActorsAction
 class twoPlayerControls(ControlActorsAction):
-    def __init__(self, keyboard_service):
+    def __init__(self, keyboard_service,player,controls):
         super().__init__(keyboard_service)
         self._keyboard_service2 = keyboard_service
         self._direction2 = Point(constants.CELL_SIZE, 0)
+        self._player = player
+        self._controls = controls
     def execute(self, cast, script):
         """Executes the control actors action.
 
@@ -16,39 +18,39 @@ class twoPlayerControls(ControlActorsAction):
         """
        #Player 1 Controls
         # left
-        if self._keyboard_service.is_key_down('a'):
+        if self._keyboard_service.is_key_down(self._controls[0]):
             self._direction = Point(-constants.CELL_SIZE, 0)
         
         # right
-        if self._keyboard_service.is_key_down('d'):
+        if self._keyboard_service.is_key_down(self._controls[1]):
             self._direction = Point(constants.CELL_SIZE, 0)
         
         # up
-        if self._keyboard_service.is_key_down('w'):
+        if self._keyboard_service.is_key_down(self._controls[2]):
             self._direction = Point(0, -constants.CELL_SIZE)
         
         # down
-        if self._keyboard_service.is_key_down('s'):
+        if self._keyboard_service.is_key_down(self._controls[3]):
             self._direction = Point(0, constants.CELL_SIZE)
         
-        bike = cast.get_first_actor("player_1")
+        bike = cast.get_first_actor(self._player)
         bike.turn_head(self._direction)
         
-        #Player 2 Controls
-        if self._keyboard_service2.is_key_down('j'):
-            self._direction2 = Point(-constants.CELL_SIZE, 0)
+        # #Player 2 Controls
+        # if self._keyboard_service2.is_key_down('j'):
+        #     self._direction2 = Point(-constants.CELL_SIZE, 0)
         
-        # right
-        if self._keyboard_service2.is_key_down('l'):
-            self._direction2 = Point(constants.CELL_SIZE, 0)
+        # # right
+        # if self._keyboard_service2.is_key_down('l'):
+        #     self._direction2 = Point(constants.CELL_SIZE, 0)
         
-        # up
-        if self._keyboard_service2.is_key_down('i'):
-            self._direction2 = Point(0, -constants.CELL_SIZE)
+        # # up
+        # if self._keyboard_service2.is_key_down('i'):
+        #     self._direction2 = Point(0, -constants.CELL_SIZE)
         
-        # down
-        if self._keyboard_service2.is_key_down('k'):
-            self._direction2 = Point(0, constants.CELL_SIZE)
+        # # down
+        # if self._keyboard_service2.is_key_down('k'):
+        #     self._direction2 = Point(0, constants.CELL_SIZE)
         
-        bike = cast.get_first_actor("player_2")
-        bike.turn_head(self._direction2)
+        # bike = cast.get_first_actor("player_2")
+        # bike.turn_head(self._direction2)
